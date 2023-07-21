375a1485e873ddb4f640d98879442ef9950d6638
Jimmy <zinuhe@outlook.com>
2023-07-20 23:24:10 -0400

    Added LisNode class

--- Problems/AddTwoNumbers/Solution.cs
+++ Problems/AddTwoNumbers/Solution.cs
@@ -7,5 +7,18 @@ namespace Problems.AddTwoNumbers
 		{
 		}
 	}
+
+
+    //Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode? next;
+        public ListNode(int val = 0, ListNode? next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }
 
