12c3463dc45a11da712d706e13221fe1f2fced42
Jimmy <zinuhe@outlook.com>
2023-09-07 23:06:44 -0400

    Bug fixing

--- RemoveElement/RemoveElement/Solution.cs
+++ RemoveElement/RemoveElement/Solution.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
 
 namespace RemoveElement;
 public class Solution
@@ -10,43 +7,37 @@ public class Solution
     {
         Console.WriteLine("Hello, Remove Element!");
 
-        int[] input = { 1, 2, 3, 4 };
-        // var result = RemoveElement(input, 3);
-        // Console.WriteLine("result:" + result);
-        Console.WriteLine("input: " + string.Join(",", input));
-        var tmp = RemoveElement(input, 10);
-        // Console.WriteLine("input: " + string.Join(",", input));
+        int[] input = { 1, 2, 2, 4, 5, 9 }; // Input array
+        int valToRemove = 2; // Value to remove from the array
+
+        Console.WriteLine($"Input : {string.Join(",", input)}, Value to remove = {valToRemove}");
+        int k = RemoveDuplicatesAndShiftLeft(input, valToRemove);
+        Console.WriteLine($"Output: {string.Join(",", input)}, k = {k}");
     }
 
-    public static int RemoveElement(int[] nums, int val)
+
+    public static int RemoveDuplicatesAndShiftLeft(int[] array, int val)
     {
-        // var array = nums.Except(new int[] { val }).ToArray();
-        // return nums.Length - array.Length;
-        // Console.WriteLine("array:" + string.Join(",", array));
-        // Console.WriteLine("k=" + (nums.Length - array.Length - 1));
+        int countRemoved = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == val)  // found value to remove
+            {
+                ShiftLeft(array, i); // call the funtion to shift
+                i--;
+                countRemoved++;
+            }
+        }
 
-        // for (int i = 0; i < nums.Length; i++)
-        // {
-        //     if (nums[i] == val)
-        //     {
-        //         nums[i] = nums[i - 1];
-        //         //shift left
-        //     }
-        // }
-
-        nums[0] = val;
-        Console.WriteLine("nums: " + string.Join(",", nums));
-        ShiftLeft(nums, 0);
-        Console.WriteLine("nums: " + string.Join(",", nums));
-        return 0;
+        return array.Length - countRemoved;
     }
 
-    public static void ShiftLeft(int[] array, int index)
+    private static void ShiftLeft(int[] array, int index)
     {
         for (int i = index; i < array.Length - 1; i++)
         {
             array[i] = array[i + 1];
         }
+        array[^1] = 0; // array[array.Length] - replacing by 0 the last position
     }
 }
-
